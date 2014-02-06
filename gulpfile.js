4b802fae4addb4c7dcca82456db2d517ea096e2b
tmaximini <tmaximini@gmail.com>
2014-02-06 14:29:33 +0100

    gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,14 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
+var pkg = require('./package.json');
 
-gulp.task('scripts', function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  return gulp.src(['client/js/**/*.js', '!client/js/vendor/**'])
+gulp.task('build', function () {
+  return gulp.src('./src/*.js')
+    .pipe(concat(pkg.name + '.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename(pkg.name + '.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
-});
-
-// Copy all static images
-gulp.task('images', function() {
- return gulp.src('client/img/**')
-    // Pass in options to the task
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-});
-
-// The default task (called when you run `gulp`)
-gulp.task('default', function() {
-  gulp.run('scripts', 'images');
-
-  // Watch files and run tasks if they change
-  gulp.watch('client/js/**', function() {
-    gulp.run('scripts');
-  });
-
-  gulp.watch('client/img/**', function() {
-    gulp.run('images');
-  });
+    .pipe(gulp.dest('./dist'));
 });
